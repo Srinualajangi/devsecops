ff95174491233379ccfcac4deb32d893c1371c87
DevSecOps Learner <srinualajangi99@gmail.com>
2026-02-05 16:27:25 +0000

    fix: rename ENV vars to avoid K8s conflicts

--- source-code/payment/payment.py
+++ source-code/payment/payment.py
@@ -21,9 +21,9 @@ app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 
 CART = os.getenv('CART_HOST', 'cart')
-CART_PORT = os.getenv('CART_PORT', 8080)
+CART_PORT = os.getenv("SHOP_CART_PORT", 8080)
 USER = os.getenv('USER_HOST', 'user')
-USER_PORT = os.getenv('USER_PORT', 8080)
+USER_PORT = os.getenv("SHOP_USER_PORT", 8080)
 PAYMENT_GATEWAY = os.getenv('PAYMENT_GATEWAY', 'https://google.com/')
 
 # Prometheus
